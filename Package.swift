b71d424d27f15bd7de982000bcab728ae99f1e55
sunkeydokey <sunkeydokey@gmail.com>
2026-05-21 16:22:00 +0900

    chore: configure SunKit target paths

--- Package.swift
+++ Package.swift
@@ -5,22 +5,26 @@ import PackageDescription
 
 let package = Package(
     name: "SunKit",
+    platforms: [
+        .iOS(.v18),
+        .tvOS(.v18),
+        .macOS(.v15),
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "SunKit",
             targets: ["SunKit"]
         ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "SunKit"
+            name: "SunKit",
+            path: "Sources"
         ),
         .testTarget(
             name: "SunKitTests",
-            dependencies: ["SunKit"]
+            dependencies: ["SunKit"],
+            path: "Tests"
         ),
     ],
     swiftLanguageModes: [.v6]
